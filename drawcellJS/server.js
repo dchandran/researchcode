bb0f9979ac87cb7c15eed3d4c740681396d9190c
Deepak Chandran <dhandran1@gmail.com>
2015-05-28 11:08:27 -0700

    python runs

--- drawcellJS/server.js
+++ drawcellJS/server.js
@@ -9,27 +9,13 @@ var app = express()
 app.use(bodyParser.json({ limit: '5mb' }))
 
 var fileServer = new static.Server('./');
- 
-require('http').createServer(function (request, response) {
-    var parsedUrl = url.parse(request.url);
-    var query, code;
+var parsedUrl, query, code;
+
+require('http').createServer(function (request, response) {    
+    parsedUrl = url.parse(request.url);
+
     if (parsedUrl.pathname==='/api') {
-        try {
-            //console.log(request);
-            code = decodeURI(parsedUrl.query);
-            fs.writeFile("temp.py", code, function(err) {
-                if(err) {
-                    console.log("Write File Error:" + err);
-                }
-                exec('python temp.py', function (error, stdout, stderr) {
-                  console.log(stdout);
-                  console.log("Python Error: " + stderr);
-                });
-            }); 
-            
-        } catch (err) {
-            console.log("Exception: " + err);
-        }
+
     } else {
         request.addListener('end', function () {
             fileServer.serve(request, response);
@@ -38,7 +24,25 @@ require('http').createServer(function (request, response) {
 
     request.setEncoding('utf8');
     request.on('data', function (chunk) {
-       console.log(chunk);
+        parsedUrl = url.parse(request.url);
+        
+        if (parsedUrl.pathname==='/api') {
+            try {
+                code = decodeURI(chunk);
+                fs.writeFile("temp.py", code, function(err) {
+                    if(err) {
+                        console.log("Write File Error:" + err);
+                    }
+                    exec('python3 temp.py', function (error, stdout, stderr) {
+                      console.log(stdout);
+                      console.log("Python Error: " + stderr);
+                    });
+                }); 
+                
+            } catch (err) {
+                console.log("Exception: " + err);
+            }
+        }
     });
 
 }).listen(1337, '127.0.0.1');
