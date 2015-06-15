7d4ce3f6d727e3e89f6102f39982f1ed2c22c247
Deepak Chandran <dhandran1@gmail.com>
2015-06-15 12:07:18 -0700

    not sure what this commit is!

---
+++ seq_pipe/parallel_primer3.sh
@@ -0,0 +1,7 @@
+rm -rf inupts
+rm -rf outputs
+mkdir inputs
+mkdir outputs
+mkdir outputs/good
+mkdir outputs/bad
+cat candidate_seqs.txt | parallel --block 100k --blocksize 1060493 --recstart '>' --pipe python3 run_primer3_on_stdin.py
---
+++ seq_pipe/run_primer3_on_stdin.py
@@ -0,0 +1,59 @@
+from back_translate import backtranslate
+from Bio.Seq import Seq
+import sys
+import subprocess
+import os.path
+
+
+prot_seq = "IAMSYNTHETIC"
+dna_seqs = backtranslate(prot_seq)
+
+def build_primer3_input(forward, reverse, filename):
+    primer_input_file = open(filename,'w');
+    s = "SEQUENCE_PRIMER=" + forward + "\n" + \
+         "SEQUENCE_PRIMER_REVCOMP=" + reverse + "\n" + \
+         "PRIMER_TASK=check_primers\nPRIMER_EXPLAIN_FLAG=1\nPRIMER_PAIR_EXPLAIN=1\nPRIMER_MIN_SIZE=5\nPRIMER_MAX_SIZE=36\nPRIMER_MAX_SELF_ANY=1\n="
+    primer_input_file.write(s)
+    primer_input_file.close()
+
+def check_all_reverse_primers(forward):
+    global dna_seqs
+
+    n = len(dna_seqs)
+    for i in range(0,n):
+        forward = forward.replace('\n','')
+
+        if not dna_seqs[i] == forward:
+            reverse = str(Seq(dna_seqs[i]).complement())
+
+            inputfile = 'inputs/' + forward + '.' + reverse + '.txt'
+            outputfile = 'outputs/' + forward + '.' + reverse + '.txt'
+
+            build_primer3_input(forward, reverse, inputfile)
+            os.system('primer3 < ' + inputfile + ' > ' + outputfile)
+            out = open(outputfile)
+            n = len(out.readlines())
+            out.close()
+            if n <= 10:
+                os.system('mv ' + outputfile + ' outputs/good')
+            else:
+                os.system('mv ' + outputfile + ' outputs/bad')
+            
+
+
+for line in sys.stdin:
+    forward = line.replace('\n','')
+    check_all_reverse_primers(forward)
+
+    
+    reverse = str(Seq(forward))
+
+    inputfile = 'inputs/' + forward + '.txt'
+    outputfile = 'outputs/' + forward + '.txt'
+
+    build_primer3_input(forward, reverse, inputfile)
+    os.system('primer3 < ' + inputfile + ' > ' + outputfile)
+    out = open(outputfile)
+    if len(out.readlines()) <= 10:
+        os.system('rm ' + outputfile)
+    out.close()
