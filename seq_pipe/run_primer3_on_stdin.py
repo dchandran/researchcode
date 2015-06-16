394161d9842d2941eddcffc55b6c7f2ca84f9481
Deepak Chandran <dhandran1@gmail.com>
2015-06-16 10:30:21 -0700

    filter by GC first

--- seq_pipe/run_primer3_on_stdin.py
+++ seq_pipe/run_primer3_on_stdin.py
@@ -1,5 +1,6 @@
 from back_translate import backtranslate
 from Bio.Seq import Seq
+from Bio.SeqUtil import GC
 import sys
 import subprocess
 import os.path
@@ -7,14 +8,16 @@ import os.path
 _INPUTFOLDER = "."
 _OUTPUTFOLDER = "."
 
+#create the primer3 input file from the given forward and reverse primers
 def build_primer3_input(forward, reverse, filename):
     primer_input_file = open(filename,'w');
     s = "SEQUENCE_PRIMER=" + forward + "\n" + \
          "SEQUENCE_PRIMER_REVCOMP=" + reverse + "\n" + \
-         "PRIMER_TASK=check_primers\nPRIMER_EXPLAIN_FLAG=1\nPRIMER_PAIR_EXPLAIN=1\nPRIMER_MIN_SIZE=5\nPRIMER_MAX_SIZE=36\nPRIMER_MAX_SELF_ANY=1\n="
+         "PRIMER_TASK=check_primers\nPRIMER_EXPLAIN_FLAG=1\nPRIMER_PAIR_EXPLAIN=1\nPRIMER_MIN_SIZE=5\nPRIMER_MAX_SIZE=36\nPRIMER_THERMODYNAMIC_PARAMETERS_PATH=/usr/share/primer3-2.3.6/src/primer3_config/\n="
     primer_input_file.write(s)
     primer_input_file.close()
 
+#checks the given forward sequence argument against all of the dna sequences provided as the second argument
 def check_all_reverse_primers(forward, dna_seqs):
     global _INPUTFOLDER
     global _OUTPUTFOLDER
@@ -24,22 +27,37 @@ def check_all_reverse_primers(forward, dna_seqs):
 
     n = len(dna_seqs)
     for i in range(0,n):
-        forward = forward.replace('\n','')
 
+
+        forward = forward.replace('\n','')
         reverse = str(Seq(dna_seqs[i]).complement())
 
-        inputfile = _INPUTFOLDER + '/' + forward + '.' + reverse + '.in'
-        outputfile = _OUTPUTFOLDER + '/' + forward + '.' + reverse + '.out'
+        gc1 = GC(forward)
+        gc2 = GC(reverse)
 
-        build_primer3_input(forward, reverse, inputfile)
-        os.system('primer3 < ' + inputfile + ' > ' + outputfile)
-        out = open(outputfile)
-        n = len(out.readlines())
-        out.close()
-        if n <= 10:
-            os.system('mv ' + outputfile + ' ' + _OUTPUTFOLDER + '/good')
-        else:
-            os.system('mv ' + outputfile + ' ' + _OUTPUTFOLDER + '/bad')
+        #first, screen using GC content before running primer3
+        if gc1 > 0.4 and gc < 0.6 and gc2 > 0.4 and gc2 < 0.6:
+
+            #input and output file locations for primer 3
+            inputfile = _INPUTFOLDER + '/' + forward + '.' + reverse + '.in'
+            outputfile = _OUTPUTFOLDER + '/' + forward + '.' + reverse + '.out'
+
+            #create primer3 input file
+            build_primer3_input(forward, reverse, inputfile)
+
+            #RUN PRIMER3
+            os.system('primer3 < ' + inputfile + ' > ' + outputfile)
+
+            #parse the output file and check for errors
+            out = open(outputfile)
+            n = len(out.readlines())
+            out.close()
+            #if more lines then input file, then there must be errors -- not sure if this is too strict!
+            if n <= 10:  
+                os.system('mv ' + outputfile + ' ' + _OUTPUTFOLDER + '/good')
+            else:
+                os.system('rm ' + outputfile)
+                #os.system('mv ' + outputfile + ' ' + _OUTPUTFOLDER + '/bad')
 
 # -------------
 # MAIN PROGRAM
@@ -48,7 +66,7 @@ if len(sys.argv) < 3:
     print ("command-line args: protein-sequence, input-folder, output-folder. std input: DNA sequence")
 else:
     _INPUTFOLDER = sys.argv[2]
-    _OUTPUTFOLDER = sys.argv[2]
+    _OUTPUTFOLDER = sys.argv[3]
     prot_seq = sys.argv[1]
     dna_seqs = backtranslate(prot_seq)
 
