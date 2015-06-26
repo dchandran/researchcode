132130fff8a19b45795b96f7cf37f0aca0c0e3c9
Deepak Chandran <dhandran1@gmail.com>
2015-06-25 22:25:41 -0700

    r clustering working

--- seq_pipe/dockerbuild/stage1_pipeline.r
+++ seq_pipe/dockerbuild/stage1_pipeline.r
@@ -1,7 +1,8 @@
-library("seqinr");
-library("cluster");
-library("fpc");
+library("seqinr")
+library("cluster")
+library("fpc")
 
+#load fasta and count k-mers
 fas <- read.fasta(file = "reads.fasta")
 n = length(fas)
 k = 2
@@ -10,8 +11,34 @@ for (i in 1:n) {
   kmers[i,] = count(fas[[i]], k)
 }
 
-save(".RData")
-#pamk.best <- pamk(kmers,krange= (1:10 * 10))
-#clusters = kmeans( kmers,  pamk.best$nc )
+#Identify the best cluster size
+koptions = c(5,10,20,30,50,80,100,200)
+n = length(koptions)
+explainedVar = matrix(0,n,2)
+for (i in 1:n) {
+  clusters = kmeans( kmers, koptions[i] , 100);
+  explainedVar[i,1] = koptions[i]
+  explainedVar[i,2] = clusters$betweenss/clusters$totss
+}
+plot(explainedVar)
 
+#Final clustering
+numClusters = 50
+finalClusters = kmeans( kmers, numClusters , 100);
+
+#Group FASTA sequences
+fastaGroups = list()
+for (i in 1:numClusters) {
+  fastaGroups[[i]] = list()
+}
+
+n = length(fas)
+for (i in 1:n) {
+  cluster = finalClusters$cluster[i]
+  m = length(fastaGroups[[cluster]])
+  fastaGroups[[cluster]][[m+1]] = fas[[i]]
+}
+
+
+save.image(".RData")
 
