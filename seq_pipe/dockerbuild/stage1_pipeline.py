bec796f98f937acd4c4c610c5845454cd74d7ef9
Deepak Chandran <deepak.chandran@autodesk.com>
2015-06-30 09:54:17 -0700

    small changes to py script for stage1 pipeline

--- seq_pipe/dockerbuild/stage1_pipeline.py
+++ seq_pipe/dockerbuild/stage1_pipeline.py
@@ -3,10 +3,22 @@ import sys
 import os
 
 lines = sys.stdin.readlines()
+#for each contig file name (stdin from pipe)
 for line in lines:
     line.replace("\n","")
-    #predict genes
+
     filename = "prod"
+
+    #predict genes in that file using Prodigal
     os.system("prodigal -i " + line + " -f gff -p meta -o " + filename + ".gff")
+
+    #convert the gff output from prodigal to fasta sequences 
     os.system("bedtools getfasta -fi " + line +  " -bed prod.gff -fo " + filename + ".fa")
+
+    #cluster all the sequences to extract unique CDS
     os.system("cd-hit -i " + filename + ".fa -o " +  filename + ".fasta")
+
+    #print out the unique CDS - later used for Blast
+    fin = open(filename + ".fasta")
+    
+    fin.close()
