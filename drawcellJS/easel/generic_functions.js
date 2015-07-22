afb3dd9485a99bba6d357b1f4219a38ab993fc50
Deepak Chandran <deepak.chandran@autodesk.com>
2015-07-22 10:48:53 -0700

    added generic func js

---
+++ drawcellJS/easel/generic_functions.js
@@ -0,0 +1,49 @@
+function initDiffusableMolecule(m, bounds, rotate) {
+    if (rotate===undefined) {
+        rotate = true;
+    }
+
+    if (rotate) {
+        m.rotation = 360*Math.random();
+    }
+
+    m.allowRotation = rotate;
+    m.velX = 0.7*(Math.random() - 0.5);
+    m.velY = 0.7*(Math.random() - 0.5);
+    m.velTheta = (Math.random() - 0.5);
+    m.bounds = bounds;
+}
+
+function moveDiffusableMolecule(m) {
+    m.x = m.x + m.velX;
+    m.y = m.y + m.velY;
+    var right = m.bounds.left + m.bounds.width;
+    var bottom = m.bounds.top + m.bounds.height;
+
+    if (m.x > right) {
+        m.x = right;
+        m.velX = - m.velX;
+    }
+    if (m.x < m.bounds.left) {
+        m.x = m.bounds.left;
+        m.velX = - m.velX;
+    }
+    if (m.y < m.bounds.top) {
+        m.y = m.bounds.top;
+        m.velY = - m.velY;
+    }
+    if (m.y > bottom) {
+        m.y = bottom;
+        m.velY = - m.velY;
+    }
+
+    if (m.allowRotation) {
+        m.rotation = m.rotation + m.velTheta;
+        if (m.rotation < 0) m.rotation = 360;
+        if (m.rotation > 360) m.rotation = 0;
+
+        if (Math.random() < 0.01) {
+            m.velTheta = Math.random() - 0.5;
+        }
+    }
+}
